fcd87a31e174c852fc2a1a324f7c7c5cb0bdf21c
SathvikY3533 <sathvik.y007@gmail.com>
2022-10-16 10:02:56 -0500

    commented 'expor' out to make code work

--- createQuestion.js
+++ createQuestion.js
@@ -5,7 +5,7 @@ ans1 = search.get('ans1');
 ans2 = search.get('ans2');
 ans3 = search.get('ans3');
 ans4 = search.get('ans4');
-export const correct = search.get('correctAnswer');
+//export const correct = search.get('correctAnswer');
 
 
 add.innerHTML += "<br><br><form action='submitPage2.html' method='GET'> <section class='question'> <span>" + qInput + "</span> <br> <input type='radio' id='"+ans1+"' name='q2' value='"+ans1+"' required> <label for='"+ans1+"'>"+ans1+"</label> <br> <input type='radio' id='"+ans2+"' name='q2' value='"+ans2+"' required> <label for='"+ans2+"'>"+ans2+"</label> <br> <input type='radio' id='"+ans3+"' name='q2' value='"+ans3+"' required> <label for='"+ans3+"'>"+ans3+"</label> <br> <input type='radio' id='"+ans4+"' name='q2' value='"+ans4+"' required> <label for='"+ans4+"'>"+ans4+"</label> <br><br> </section> <section class='submit'> <button type='submit'>Submit</button> </section> </form>";
